3f9658e4969c106e3f819e5e5c902efb01e95617
bselpin <bselpin@naver.com>
2022-01-09 19:59:08 +0900

    no message

--- src/lib/hooks/useGoogleAdsense.tsx
+++ src/lib/hooks/useGoogleAdsense.tsx
@@ -1,34 +1,16 @@
 import { useCallback, useEffect } from "react"
 
 export const useGoogleAdsense = () => {
-	const loadAd = useCallback(() => {
-		try {
-			const ads = (window.adsbygoogle = window.adsbygoogle || [])
-			ads.push({
-				google_ad_client: process.env.NEXT_PUBLIC_GOOGLE_ADSENSE,
-				enable_page_level_ads: true,
-			})
-		} catch (error) {
-			console.error(error)
-		}
-	}, [])
-
 	useEffect(() => {
 		if (typeof window !== "undefined") {
 			const component = window.document
 				.getElementById("__next")
 				.querySelector(`.adsbygoogle`)
 			if (component) {
-				component.addEventListener("load", loadAd)
-			}
-		}
-		return () => {
-			if (typeof window !== "undefined") {
-				const component = window.document
-					.getElementById("__next")
-					.querySelector(`.adsbygoogle`)
-				if (component) {
-					component.removeEventListener("load", loadAd)
+				try {
+					;(window.adsbygoogle = window.adsbygoogle || []).push({})
+				} catch (error) {
+					console.error(error)
 				}
 			}
 		}
